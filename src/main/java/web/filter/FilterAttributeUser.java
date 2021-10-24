1d074624dbb9042642c9104c09f297899a88540a
Dimon <dima2000@tut.by>
2021-10-24 21:00:46 +0300

    add

--- src/main/java/web/filter/FilterAttributeUser.java
+++ src/main/java/web/filter/FilterAttributeUser.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebFilter(servletNames = {"edit"})
 public class FilterAttributeUser extends HttpFilter {
 
